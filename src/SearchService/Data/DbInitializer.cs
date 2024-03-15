6900ce7d0f9631caf00b1ac7dbc08b08a1faee82
MoShabayk <alshabayk123@gmail.com>
2024-03-16 00:40:11 +0100

    done with SearchService -for now-

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -19,20 +19,14 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count ==0)
-        {
-            Console.WriteLine("Seeding database...");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
-            await DB.SaveAsync(items);
-        }
+        using var scope = app.Services.CreateScope();
     
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " items received from AuctionService");
+
+        if (items.Count > 0) await DB.SaveAsync(items); 
     }
 }
