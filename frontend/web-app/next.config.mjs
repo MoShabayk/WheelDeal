7313c1cbb00d699e22e27d357e94f4e4e63d67d4
MoShabayk <alshabayk123@gmail.com>
2024-04-10 03:00:35 +0200

    Client, BiddingService, NotificationService

---
+++ frontend/web-app/next.config.mjs
@@ -0,0 +1,8 @@
+const nextConfig = {
+  images: {
+    domains: ['cdn.pixabay.com']
+  },
+  output: 'standalone'
+}
+
+export default nextConfig;
--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -8,20 +8,19 @@ namespace SearchService;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item,Item>();
+        var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
-        if(!string.IsNullOrEmpty(searchParams.SearchTerm))
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
         query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "make" => query.Sort(x => x.Ascending(a => a.Make))
+                .Sort(x => x.Ascending(a => a.Model)),
             "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
             _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
         };
@@ -29,17 +28,17 @@ public class SearchController : ControllerBase
         query = searchParams.FilterBy switch
         {
             "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) 
-            && x.AuctionEnd > DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                && x.AuctionEnd > DateTime.UtcNow),
             _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
         };
 
-        if(!string.IsNullOrEmpty(searchParams.Seller))
+        if (!string.IsNullOrEmpty(searchParams.Seller))
         {
             query.Match(x => x.Seller == searchParams.Seller);
         }
 
-        if(!string.IsNullOrEmpty(searchParams.Winner))
+        if (!string.IsNullOrEmpty(searchParams.Winner))
         {
             query.Match(x => x.Winner == searchParams.Winner);
         }
@@ -51,10 +50,9 @@ public class SearchController : ControllerBase
 
         return Ok(new
         {
-            result = result.Results,
+            results = result.Results,
             pageCount = result.PageCount,
             totalCount = result.TotalCount
         });
     }
-
 }
